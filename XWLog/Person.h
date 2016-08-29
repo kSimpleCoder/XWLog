705e2170d49fc13fff0c9cf94ff5809dea35d627
LXW <1066989400@qq.com>
2016-08-29 15:26:05 +0800

    first commit

--- XWLog/Person.h
+++ XWLog/Person.h
@@ -10,9 +10,21 @@
 
 @interface Person : NSObject
 
-@property (nonatomic, copy) NSString *name;
+/**
+ *  年龄
+ */
+@property (nonatomic, assign) int age;
+/**
+ *  性别
+ */
 @property (nonatomic, copy) NSString *gender;
-@property (nonatomic, assign) NSInteger age;
+/**
+ *  名字
+ */
+@property (nonatomic, copy) NSString *name;
+/**
+ *  地址
+ */
 @property (nonatomic, copy) NSString *address;
 
 @end
--- XWLog/ViewController.m
+++ XWLog/ViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "ViewController.h"
+#import "MJExtension.h"
+#import "Person.h"
+#import "NSObject+Log.h"
 
 @interface ViewController ()
 
@@ -16,7 +19,37 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+/*注：如果只用到打印模型的方法 不需要引入"NSObject+Log.h"*/
+    [self descriptionTest1];
+//    [self descriptionTest2];
+//    [self propertiesTest];
+    
+}
+
+// NO.1 检查赋值信息 (直接打印模型)
+- (void)descriptionTest1 {
+    Person *person = [[Person alloc] init];
+    person.name    = @"Lucy";
+    person.age     = 27;
+    person.gender  = @"女";
+    person.address = @"北京-朝阳";
+    NSLog(@"%@",person);
+}
+
+// NO.2 配合网络解析 检查赋值情况(直接打印模型)
+- (void)descriptionTest2 {
+    NSString *jsonString = @"{\"name\":\"Jack\", \"age\":28, \"gender\":\"男\", \"address\":\"北京-朝阳\",}";
+    Person *person = [Person mj_objectWithKeyValues:jsonString];
+    NSLog(@"%@",person);
+}
+
+// NO.3 任意对象调用 propertiesCodeWithDict 传入想要生成属性的字典即可
+- (void)propertiesTest {
+    NSString *jsonString = @"{\"name\":\"Jack\", \"age\":28, \"gender\":\"男\", \"address\":\"北京-朝阳\",}";
+    NSData *data = [jsonString dataUsingEncoding:NSUTF8StringEncoding];
+    NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:nil];
+    [NSObject propertiesCodeWithDict:dict];
 }
 
 - (void)didReceiveMemoryWarning {
